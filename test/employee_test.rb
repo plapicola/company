d5f7cb3ffa38072911fcd583ef3e1f9351bc1007
Brian Zanti <brianzanti@gmail.com>
2018-07-27 08:51:30 -0600

    initial commit

---
+++ test/employee_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/employee'
+
+class employeeTest < Minitest::Test
+  def test_it_exists
+		employee = Employee.new({name: "Alice Smith", age: 20, salary: "50000"})
+
+		assert_instance_of Employee, employee
+	end
+
+	def test_it_has_name
+		employee = Employee.new({name: "Alice Smith", age: 20, salary: "50000"})
+
+		assert_equal "Alice Smith", employee.name
+	end
+
+	def test_it_has_age
+		employee = Employee.new({name: "Alice Smith", age: 20, salary: "50000"})
+
+		assert_equal 20, employee.age
+	end
+
+	def test_it_has_salary
+		employee = Employee.new({name: "Alice Smith", age: 20, salary: "50000"})
+
+		assert_equal 50000, employee.salary
+	end
+end
